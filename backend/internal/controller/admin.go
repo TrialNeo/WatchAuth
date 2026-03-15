237dfe22b38a5fd9dad5504f0b1b972b5740c308
Trial <shenpanpro@gmail.com>
2026-03-15 23:07:01 +0800

    fix the problem of login
    Signed-off-by: Trial <shenpanpro@gmail.com>

--- backend/internal/controller/admin.go
+++ backend/internal/controller/admin.go
@@ -22,7 +22,7 @@ func (a *AdminController) Login(c *fiber.Ctx) error {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"msg": err.Error()})
 	}
 	resp := a.Service.Login(login.Username, login.Password, c.IP())
-	return Respond(c, resp.Code, resp.ErrMsg)
+	return Respond(c, resp.Code, resp)
 }
 
 type menu struct {
